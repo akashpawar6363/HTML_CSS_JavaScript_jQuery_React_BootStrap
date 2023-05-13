2876876a729770fbdacb0bf2ac263995c28838ee
Akash Pawar <131381080+akashpawar6363@users.noreply.github.com>
2023-05-13 16:44:45 +0530

    new to upload

--- 12 may/guestrouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-let guestrouter = Router();
-export default guestrouter;
-
-let hostname = 'do while'
-
-guestrouter.get('/' , (req, res) =>{
-    res.send(`this is from ${hostname} Boys`);
-})
